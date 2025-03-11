3599c69c12e7873a8287ddc3c9ae82667d634d06
miixkyy2611 <miguel.angelmi@hotmail.com>
2025-03-11 13:40:51 -0600

    subeteeee

--- backBase/index.js
+++ backBase/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const cookieParser = require('cookie-parser'); // Importar cookie-parser
+const cookieParser = require('cookie-parser'); // Importar cookie-parser //a
 const dotenv = require('dotenv');
 const ipRouter = require('./routes/ip/ipRouter');
 const loginRouter = require('./routes/auth/authRoutes');
