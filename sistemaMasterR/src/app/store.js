33f30d42dfa08a49ad509f8123e30ff4b947830f
miixkyy2611 <miguel.angelmi@hotmail.com>
2025-03-19 10:22:12 -0600

    store fusionado

--- sistemaMasterR/src/app/store.js
+++ sistemaMasterR/src/app/store.js
@@ -1,13 +1,16 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
-import verificacionIp from '../accesoLogin/reducers/slice/verificacionIp/verificacionIp'
-import autenticacionLogin from '../accesoLogin/reducers/slice/loginUsuarioSlice/authSlice'
-import erroresFormulariosReducer from '../sistema/reducers/slice/erroresFormularios/ErroresFormularios'
-import erroresRegistroReducer from '../sistema/reducers/slice/erroresRegistro/ErroresRegistro'
+import verificacionIp from '../accesoLogin/reducers/slice/verificacionIp/verificacionIp';
+import autenticacionLogin from '../accesoLogin/reducers/slice/loginUsuarioSlice/authSlice';
+import erroresFormulariosReducer from '../sistema/reducers/slice/erroresFormularios/ErroresFormularios';
+import erroresRegistroReducer from '../sistema/reducers/slice/erroresRegistro/ErroresRegistro';
 import idAlternaReducer from "../sistemaAdmin/reducers/slice/idAlterna/idAlternaSlice";
+import imagenesReducer from '../sistema/redux/imagenesSlice'; // ✅ Añadir imágenes
+import huellasReducer from '../sistema/redux/huellasSlice'; // ✅ Añadir huellas
 
+// Configuración para redux-persist
 const persistConfig = {
   key: 'root',
   storage,
@@ -15,23 +18,21 @@ const persistConfig = {
 
 const persistedAuthReducer = persistReducer(persistConfig, autenticacionLogin);
 
-
 export const store = configureStore({
   reducer: {
-   
-    auth:persistedAuthReducer,
+    auth: persistedAuthReducer,  // ✅ Persistencia en auth
     verificacionIp: verificacionIp,
     erroresFormularios: erroresFormulariosReducer,
     erroresRegistro: erroresRegistroReducer,
     idAlterna: idAlternaReducer,
+    imagenes: imagenesReducer,  // ✅ Agregado imágenes
+    huellas: huellasReducer,    // ✅ Agregado huellas
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
     }),
-
-})
-
+});
 
 export const persistor = persistStore(store);
 export default store;
