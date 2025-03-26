547af5ec6aab29dab8c87157932106a16e378711
Emmanuel <emmanuelescobedo0999@gmail.com>
2025-03-26 14:27:07 -0600

    Domicilio

--- sistemaMasterR/src/sistemaAdmin/componentes/bloqueUno/Domicilio.jsx
+++ sistemaMasterR/src/sistemaAdmin/componentes/bloqueUno/Domicilio.jsx
@@ -7,85 +7,97 @@ import ValidacionBloqueUno from '../../../sistema/validaciones/validacionBloque1
 import useDatosGeneralesStore from '../../zustand/useDatosGeneralesStore';
 import useStore from '../../zustand/useStore';
 
-const Domicilio = ({ data, onFormChange, onValidationStatus }) => {
+const Domicilio = ( { data, onFormChange, onValidationStatus } ) => {
   const { register, formState: { errors }, setError, clearErrors } = useFormContext();
   const { datos, actualizarDato, seleccionarRadio, radioSeleccionados } = useDatosGeneralesStore();
   const { domicilio, cargarDomicilio } = useStore();
 
-  const handleChange = (e) => {
+  const handleChange = ( e ) => {
     const { name, value } = e.target;
     const lowercaseName = name.toLowerCase();
-    const validationResult = ValidacionBloqueUno[`validacion${capitalizeFirstLetter(lowercaseName)}`](value);
+    const validationResult = ValidacionBloqueUno[ `validacion${ capitalizeFirstLetter( lowercaseName ) }` ]( value );
 
-    if (validationResult !== true) {
-      setError(name, { type: 'formulario 1', message: validationResult });
+    if ( validationResult !== true ) {
+      setError( name, { type: 'formulario 1', message: validationResult } );
     } else {
-      clearErrors(name);
+      clearErrors( name );
     }
-    onFormChange(name, value);
+    onFormChange( name, value );
   };
 
-  const handleRadioChange = (nombre, valor, formulario) => {
-    seleccionarRadio(nombre, valor, formulario);
+  const handleRadioChange = ( nombre, valor, formulario ) => {
+    seleccionarRadio( nombre, valor, formulario );
   };
 
-  const capitalizeFirstLetter = (string) => {
-    return string.charAt(0).toUpperCase() + string.slice(1);
+  const capitalizeFirstLetter = ( string ) => {
+    return string.charAt( 0 ).toUpperCase() + string.slice( 1 );
   };
 
-  useEffect(() => {
-    onValidationStatus(errors);
-  }, [errors, onValidationStatus]);
+  useEffect( () => {
+    onValidationStatus( errors );
+  }, [ errors, onValidationStatus ] );
 
-  const renderTooltip = (message) => (
-    <Tooltip>{message}</Tooltip>
+  const renderTooltip = ( message ) => (
+    <Tooltip>{ message }</Tooltip>
   );
 
-  const domicilioObtenidos = domicilio?.[0] || {};
+  const domicilioObtenidos = domicilio?.[ 0 ] || {};
   //console.log(domicilioObtenidos);
+
+  // Manejo de selección/deselección de checkboxes
+  const handleCheckboxChange = ( label, valor ) => {
+    if ( radioSeleccionados.some( item => item.nombre === label && item.valor === valor ) ) {
+      seleccionarRadio( label, null, 'Domicilio' );
+    } else {
+      seleccionarRadio( label, valor, 'Domicilio' );
+    }
+  };
 
   return (
     <div className="row">
-      {[
+      { [
         { id: "DMUNIC", label: "Municipio del individuo", sourceKey: "nombre_municipio" },
         { id: "DENTID", label: "Entidad federativa", sourceKey: "nombre_entidad" },
         { id: "DCOLONIA", label: "Colonia del individuo" },
         { id: "DCALLE", label: "Calle del individuo" },
         { id: "DCP", label: "Código postal" },
-      ].map((field) => (
-        <div key={field.id} className="col-md-3 form-floating mt-3 d-flex align-items-center">
+      ].map( ( field ) => (
+        <div key={ field.id } className="col-md-3 form-floating mt-3 d-flex align-items-center">
           <OverlayTrigger
             placement="right"
-            overlay={errors[field.id] ? renderTooltip(errors[field.id].message) : <></>}
+            overlay={ errors[ field.id ] ? renderTooltip( errors[ field.id ].message ) : <></> }
           >
             <input
               type="text"
-              className={`form-control ${errors[field.id] ? 'is-invalid shake' : ''}`}
-              id={field.id}
-              name={field.id}
-              placeholder={errors[field.id] ? errors[field.id].message : field.label}
+              className={ `form-control ${ errors[ field.id ] ? 'is-invalid shake' : '' }` }
+              id={ field.id }
+              name={ field.id }
+              placeholder={ errors[ field.id ] ? errors[ field.id ].message : field.label }
               value={
                 // Si tiene `sourceKey` (como en DMUNIC y DENTID), toma el valor de ahí
                 // Si no, usa el valor directo del campo
-                field.sourceKey ? (domicilioObtenidos[field.sourceKey] || '') : (domicilioObtenidos[field.id] || '')
+                field.sourceKey ? ( domicilioObtenidos[ field.sourceKey ] || '' ) : ( domicilioObtenidos[ field.id ] || '' )
               }
-              {...register(field.id, { onChange: handleChange })}
-              style={{ borderColor: errors[field.id] ? 'red' : '' }}
+              { ...register( field.id, { onChange: handleChange } ) }
+              style={ { borderColor: errors[ field.id ] ? 'red' : '' } }
             />
           </OverlayTrigger>
-          <label htmlFor={field.id} style={{ marginLeft: '10px' }}>{field.label}</label>
+          <label htmlFor={ field.id } style={ { marginLeft: '10px' } }>{ field.label }</label>
 
-          {/* Radio Button */}
-          <input
-            type="radio"
-            name={`radio-${field.id}`}
-            value="Sí"
-            className="ms-2"
-            onChange={() => handleRadioChange(field.label, 'Sí', 'Domicilio')}
-          />
+          {/* Checkbox para Selección */ }
+          <div className="d-flex justify-content-center">
+            <input
+              type="checkbox"
+              name={ `checkbox-${ field.id }` }
+              value="Sí"
+              checked={ radioSeleccionados.some( item => item.nombre === field.label && item.valor === 'Sí' ) }
+              className="ms-2"
+              onChange={ () => handleCheckboxChange( field.label, 'Sí' ) }
+            />
+          </div>
         </div>
-      ))}
-      {/* Lista de radio seleccionados */}
+      ) ) }
+      {/* Lista de radio seleccionados */ }
       {/*<div className="mt-4">
         <h5 style={{ color: 'red' }}>Campos con errores:</h5>
         <ul>
