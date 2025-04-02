1419e705a132d043a9e483bad097a1f7ff63f016
Emmanuel <emmanuelescobedo0999@gmail.com>
2025-04-02 13:53:49 -0600

    huellas sirve

--- backBase/routes/Bloque6/editarHuellasRoutes.js
+++ backBase/routes/Bloque6/editarHuellasRoutes.js
@@ -25,24 +25,34 @@ router.put('/editar/:idAlterna', upload.array('nuevaImagen', 10), async (req, re
   }
 
   try {
-    const query = 'UPDATE imagenes SET IMAGEN = ? WHERE ID_ALTERNA = ? AND GRUPO = ?;';
+    const queryImagenes = 'UPDATE imagenes SET IMAGEN = ? WHERE ID_ALTERNA = ? AND GRUPO = ?;';
+    const queryMovimientos = 'UPDATE movimientos SET procesado = 9 WHERE ID_ALTERNA = ? AND ID_BLOQUE_FUNCIONAL = 6;';
 
+    // Actualizar las imágenes
     for (let i = 0; i < nuevasImagenes.length; i++) {
       const bufferImagen = nuevasImagenes[i].buffer;
       const grupo = grupos[i];
 
-      const [result] = await pool.query(query, [bufferImagen, idAlterna, grupo]);
-      if (result.affectedRows === 0) {
+      const [resultImagen] = await pool.query(queryImagenes, [bufferImagen, idAlterna, grupo]);
+      if (resultImagen.affectedRows === 0) {
         return res.status(404).json({
           message: `No se encontró la imagen para actualizar con el ID_ALTERNA y grupo ${grupo}.`,
         });
       }
     }
 
-    res.status(200).json({ message: 'Imágenes actualizadas exitosamente.' });
+    // Actualizar el campo 'procesado' en la tabla 'movimientos'
+    const [resultMovimientos] = await pool.query(queryMovimientos, [idAlterna]);
+    if (resultMovimientos.affectedRows === 0) {
+      return res.status(404).json({
+        message: `No se encontró un movimiento para actualizar con el ID_ALTERNA ${idAlterna}.`,
+      });
+    }
+
+    res.status(200).json({ message: 'Imágenes y movimiento actualizados exitosamente.' });
   } catch (error) {
-    console.error('Error al actualizar las imágenes:', error);
-    res.status(500).json({ message: 'Error al actualizar las imágenes en la base de datos', error });
+    console.error('Error al actualizar las imágenes y movimiento:', error);
+    res.status(500).json({ message: 'Error al actualizar las imágenes y el movimiento en la base de datos', error });
   }
 });
 
